e1b076ea670377c284e0107525eb501c31f44922
calebodemus <calebodemus@gmail.com>
2014-04-24 09:50:36 +0200

    modifgit carlos

--- projet_bugs/index.php
+++ projet_bugs/index.php
@@ -3,13 +3,11 @@ session_start();
 header("Content-Type: text/html; charset=utf-8");
 require('app/server.php');
 $page = "home"; 
-<<<<<<< HEAD
-$mysqli = mysqli_connect("localhost", "root","troiswa", "bugs"); 
 
-=======
-$mysqli = mysqli_connect("localhost", "root", "troiswa", "bugs"); 
+$mysqli = mysqli_connect("localhost", "root","troiswa", "bugs"); 
 /*| Il faut vérifier si la connection mysql a reussi ! |*/
->>>>>>> e53a545b6cd9aa70be16dfc00687d26a5eaa923e
+
+
 
 $tab = array("home" => "content/home",
 			  "carte"=>"content/carte",
