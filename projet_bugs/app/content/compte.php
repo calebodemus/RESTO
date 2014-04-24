4e3abc47313bf9290e4750a2da624c701c40b048
hearsute <manuoval@gmail.com>
2014-04-24 13:59:31 +0200

    maj compte.php

--- projet_bugs/app/content/compte.php
+++ projet_bugs/app/content/compte.php
@@ -1,7 +1,16 @@
 <?php 
 
+$loginencours = $_SESSION['loginid'];
+if ($loginencours = 404)
+{
+   $messagecompte="Veuillez vous identifier pour afficher vos informations";
+}
+else
+   $messagecompte='';
 
-$comptequery = "SELECT * FROM user";
+
+
+$comptequery = "SELECT * FROM user WHERE id = '" .$loginencours."'";
 $compteres = mysqli_query($mysqli, $comptequery);
 while ( $compteligne = mysqli_fetch_assoc($compteres) )
 {
