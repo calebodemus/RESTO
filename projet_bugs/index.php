d07cb794ee51a1a12c0ae49a9071292c7531e3fe
hearsute <manuoval@gmail.com>
2014-04-24 09:53:15 +0200

    modif index.php

--- projet_bugs/index.php
+++ projet_bugs/index.php
@@ -3,13 +3,8 @@ session_start();
 header("Content-Type: text/html; charset=utf-8");
 require('app/server.php');
 $page = "home"; 
-<<<<<<< HEAD
-$mysqli = mysqli_connect("localhost", "root","troiswa", "bugs"); 
 
-=======
-$mysqli = mysqli_connect("localhost", "root", "troiswa", "bugs"); 
-/*| Il faut vérifier si la connection mysql a reussi ! |*/
->>>>>>> e53a545b6cd9aa70be16dfc00687d26a5eaa923e
+$mysqli = mysqli_connect("localhost", "root","troiswa", "bugs"); 
 
 $tab = array("home" => "content/home",
 			  "carte"=>"content/carte",
