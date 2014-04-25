9ff7b2ab10c8b4af2102247cb744667027bbdaa7
calebodemus <calebodemus@gmail.com>
2014-04-25 14:20:21 +0200

    Modif paiement.php

--- projet_bugs/app/content/paiement.php
+++ projet_bugs/app/content/paiement.php
@@ -1,3 +1,13 @@
 <?php 
-require('views/content/paiement.html');
+	if (isset($_SESSION['montant_payer']))
+		$montant_payer = $_SESSION['montant_payer'] . ' euros';
+	else
+		$montant_payer = '0 euros';
+
+	$num_carte = '';
+	$titulaire = '';
+	$date_exp = '';
+	$cryptogramme = '';
+
+	require('views/content/paiement.html');
  ?>
